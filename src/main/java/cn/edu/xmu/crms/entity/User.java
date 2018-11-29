3f547edc0da98ff62a6b8dfb297ef01581005934
Song <icecream.song@qq.com>
2018-11-29 19:13:04 +0800

    Update User.java

--- src/main/java/cn/edu/xmu/crms/entity/User.java
+++ src/main/java/cn/edu/xmu/crms/entity/User.java
@@ -1,8 +1,60 @@
 package cn.edu.xmu.crms.entity;
 
+import java.math.BigInteger;
+
 /**
  * @author SongLingbing
  * @date 2018/11/29 10:31
  */
 public class User {
+    protected BigInteger id;
+    protected BigInteger number;
+    protected String name;
+    protected String email;
+    protected String password;
+    protected Integer noticeGap;
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public BigInteger getNumber() {
+        return number;
+    }
+
+    public void setNumber(BigInteger number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getNoticeGap() {
+        return noticeGap;
+    }
+
+    public void setNoticeGap(Integer noticeGap) {
+        this.noticeGap = noticeGap;
+    }
 }
