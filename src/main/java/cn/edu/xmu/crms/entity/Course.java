9eaff6773f864cb19d226c051653fbeac8da8cef
Baddie75 <947312014@qq.com>
2018-11-29 18:29:50 +0800

    complete entity(Course,EduClass)

--- src/main/java/cn/edu/xmu/crms/entity/Course.java
+++ src/main/java/cn/edu/xmu/crms/entity/Course.java
@@ -1,8 +1,83 @@
 package cn.edu.xmu.crms.entity;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 /**
- * @author SongLingbing
- * @date 2018/11/29 14:46
+ * @author 75
+ * @date 2018/11/29
  */
 public class Course {
+    private BigInteger id;
+    private String courseName;
+    private String courseRequire;
+    private BigDecimal presentationWeight;
+    private BigDecimal questionWeight;
+    private BigDecimal reportWeight;
+    private Integer teamMaxLimit;
+    private Integer teamMinLimit;
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseRequire() {
+        return courseRequire;
+    }
+
+    public void setCourseRequire(String courseRequire) {
+        this.courseRequire = courseRequire;
+    }
+
+    public BigDecimal getPresentationWeight() {
+        return presentationWeight;
+    }
+
+    public void setPresentationWeight(BigDecimal presentationWeight) {
+        this.presentationWeight = presentationWeight;
+    }
+
+    public BigDecimal getQuestionWeight() {
+        return questionWeight;
+    }
+
+    public void setQuestionWeight(BigDecimal questionWeight) {
+        this.questionWeight = questionWeight;
+    }
+
+    public BigDecimal getReportWeight() {
+        return reportWeight;
+    }
+
+    public void setReportWeight(BigDecimal reportWeight) {
+        this.reportWeight = reportWeight;
+    }
+
+    public Integer getTeamMaxLimit() {
+        return teamMaxLimit;
+    }
+
+    public void setTeamMaxLimit(Integer teamMaxLimit) {
+        this.teamMaxLimit = teamMaxLimit;
+    }
+
+    public Integer getTeamMinLimit() {
+        return teamMinLimit;
+    }
+
+    public void setTeamMinLimit(Integer teamMinLimit) {
+        this.teamMinLimit = teamMinLimit;
+    }
 }
--- src/main/java/cn/edu/xmu/crms/entity/EduClass.java
+++ src/main/java/cn/edu/xmu/crms/entity/EduClass.java
@@ -1,8 +1,46 @@
 package cn.edu.xmu.crms.entity;
 
+import java.math.BigInteger;
+
 /**
- * @author SongLingbing
- * @date 2018/11/29 10:38
+ * @author 75
+ * @date 2018/11/29
  */
 public class EduClass {
+    private BigInteger id;
+    private Integer classNumber;
+    private String classTime;
+    private String classAddress;
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
+
+    public Integer getClassNumber() {
+        return classNumber;
+    }
+
+    public void setClassNumber(Integer classNumber) {
+        this.classNumber = classNumber;
+    }
+
+    public String getClassTime() {
+        return classTime;
+    }
+
+    public void setClassTime(String classTime) {
+        this.classTime = classTime;
+    }
+
+    public String getClassAddress() {
+        return classAddress;
+    }
+
+    public void setClassAddress(String classAddress) {
+        this.classAddress = classAddress;
+    }
 }
