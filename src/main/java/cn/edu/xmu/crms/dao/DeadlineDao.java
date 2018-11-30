28e41bd74e347acd3b5879324b3ac511e6ca1d80
Baddie75 <947312014@qq.com>
2018-11-30 20:59:22 +0800

    update object Deadline(entity, mapper)

---
+++ src/main/java/cn/edu/xmu/crms/dao/DeadlineDao.java
@@ -0,0 +1,25 @@
+package cn.edu.xmu.crms.dao;
+
+import cn.edu.xmu.crms.entity.Deadline;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigInteger;
+
+/**
+ * @author Hongqiwu
+ * @date 2018/11/30 19:45
+ */
+@Mapper
+@Repository
+public interface DeadlineDao {
+    /**
+     * 用于通过时间号获取时间对象
+     *
+     * @param id 时间号
+     * @return Course  时间对象
+     * @author Hongqiwu
+     * @date 2018/11/30 19:45
+     */
+    Deadline selectDeadlineById(BigInteger id);
+}
