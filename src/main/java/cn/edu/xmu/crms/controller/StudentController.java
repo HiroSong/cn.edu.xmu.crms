8f2de42bd58a06a73623eeaeae32a2975445abd0
Pronpipat <616687127@qq.com>
2018-12-03 15:46:16 +0800

    wrong edition 0.7

--- src/main/java/cn/edu/xmu/crms/controller/StudentController.java
+++ src/main/java/cn/edu/xmu/crms/controller/StudentController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class StudentController {
-    @GetMapping("/{studentID}")
+    @GetMapping("/students/{studentID}")
     public Teacher getBaseInfo(@PathVariable("studentID")
                                        String studentID){
         return null;
