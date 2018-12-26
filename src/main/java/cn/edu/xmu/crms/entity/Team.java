2738e316974c645c27a23149c45dba080843badf
Baddie75 <947312014@qq.com>
2018-12-26 17:01:37 +0800

    Revert "0"
    This reverts commit 89cf301fd16ca0a39c7806002017296e4094ea69.

--- src/main/java/cn/edu/xmu/crms/entity/Team.java
+++ src/main/java/cn/edu/xmu/crms/entity/Team.java
@@ -17,6 +17,7 @@ public class Team {
     private Klass klass;
     private Student leader;
     private List<Student> members;
+    private Integer klassSerial;
 
     public Integer getKlassSerial() {
         return klassSerial;
@@ -41,6 +42,10 @@ public class Team {
     public void setKlass(Klass klass) {
         this.klass = klass;
     }
+
+    public Integer getKlassSerial() { return klassSerial; }
+
+    public void setKlassSerial(Integer klassSerial) { this.klassSerial = klassSerial; }
 
     public Student getLeader() {
         return leader;
